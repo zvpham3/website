fa5e43ac1a5615499a9ff07544f46190d8495672
zvpham3 <zvpham@tamu.edu>
2025-11-03 18:35:19 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
 // Define the route
 app.get('/', (req, res) => {
     res.render('index');
